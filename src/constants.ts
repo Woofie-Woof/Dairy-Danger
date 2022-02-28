14627fa6abcc13b7411330164d98dc983e878f7e
Ian-T <ian.talamantes@binmatter.com>
2022-02-28 09:50:46 -0700

    Started Dark Chocolate implementation

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,5 @@ export enum CollectibleTypeCustom {
   COLLECTIBLE_PROBIOTIC_YOGURT = Isaac.GetItemIdByName("Probiotic Yogurt"),
   COLLECTIBLE_DROPPED_ICE_CREAM = Isaac.GetItemIdByName("Dropped Ice Cream"),
   COLLECTIBLE_BOBS_TEA = Isaac.GetItemIdByName("Bob's Tea"),
+  COLLECTIBLE_DARK_CHOCOLATE = Isaac.GetItemIdByName("Dark Chocolate"),
 }
